34f5a5efbb91ce501fdc36fc3a13ac7a073b1e72
alexhrushko <alexhrushko@gmail.com>
2023-07-01 18:32:12 +0300

    Hotfix for export

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,2 @@
+// Added like constant for easier deploy on your PC. In real project need to use .env file
 export const API_ENDPOINT = "https://dev.flixforge.com";
--- src/pages-content/home/components/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./genres-list";
-export * from "./movies-list";
